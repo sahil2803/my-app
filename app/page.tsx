860ccbbef13e84060607fb24b45b37da783fe784
Sahil Singh <sahilsingh2803@gmail.com>
2024-05-14 21:36:58 +0200

    added a quick description

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-zinc-200 to-zinc-50 dark:from-zinc-800 dark:to-zinc-700">
-      <h1 className="text-3xl font-bold mb-8">Hello Tyler</h1>
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-zinc-200 to-zinc-50 dark:from-zinc-800 dark:to-zinc-700 flex-col">
+      <h1 className="text-3xl font-bold mb-2">Hello Tyler</h1>
+      <p className="text-lg text-gray-600">Welcome to the  boring page</p>
     </main>
   );
 }
