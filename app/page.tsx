9e3c9e1b79c686c64d2d8cfef5dc8510379c59e0
Sahil Singh <sahilsingh2803@gmail.com>
2024-05-14 21:58:05 +0200

    changed dice extension and added new text

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
       <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold">Welcome to My Website</h1>
-        <p className="text-lg">Explore and enjoy!</p>
+        <h1 className="text-3xl font-bold">Welcome to my website tyler</h1>
+        <p className="text-lg">boring</p>
       </header>
       <div className="w-36 h-36 relative">
         <Image
-          src="/dice.jpg" // Assuming "dice.jpg" is the name of your image file
+          src="/dice.svg" 
           alt="A dice"
           layout="fill"
           objectFit="cover"
