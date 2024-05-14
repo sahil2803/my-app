dd6a9b2c2c80052386b069425337c1e041852b88
Sahil Singh <sahilsingh2803@gmail.com>
2024-05-14 22:00:38 +0200

    changed backgorund colour and added new text to fix hidden dice

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-purple-900 text-white">
       <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold">Welcome to my website tyler</h1>
-        <p className="text-lg">boring</p>
+        <h1 className="text-3xl font-bold">Welcome to my boring website, Tyler</h1>
+        <p className="text-lg">I did not know what to commit, so I thought maybe adding another paragraph would be cool.</p>
       </header>
       <div className="w-36 h-36 relative">
         <Image
